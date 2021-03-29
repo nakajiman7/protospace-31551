d1541c4c847eeb80e0328858707aff16d3861590
nakajiman7 <7nakajiman7@gmail.com>
2021-03-29 13:03:22 +0900

    second commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :set_prototype, only: [:edit, :show]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_root_path, except: [:index, :new, :show]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -57,6 +57,6 @@ class PrototypesController < ApplicationController
   end
 
   def contributor_confirmation
-    move_to root_path unless current_user == @prototype.user
+    redirect_to root_path unless current_user == @prototype.user
   end
 end
